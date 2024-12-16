75cef5fd0c7c5d04de36b27264a31600d6f1e0bb
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-16 16:14:02 +0200

    Back-end - add expert_requests migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('role_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
@@ -28,7 +30,13 @@ return new class extends Migration
             $table->string('token');
             $table->timestamp('created_at')->nullable();
             $table->softDeletes();
+        });
 
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamp('created_at')->nullable();
+            $table->softDeletes();
         });
 
         
@@ -41,6 +49,6 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('user_roles');
     }
 };
