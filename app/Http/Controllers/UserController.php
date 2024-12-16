b03990999b6c3d040f341d5dc3bb5b321113ed3e
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-17 00:09:48 +0200

    Back-end - update UserController update register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,16 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 class UserController extends Controller
 {
     function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
         $user = new User;
         $user->user_role_id = 1; 
         $user->name = $request->name;
@@ -19,8 +29,12 @@ class UserController extends Controller
         $user->phone_number = $request->phone_number;
         $user->password = Hash::make($request->password);
         $user->save();
+        $token = JWTAuth::fromUser($user);
+
         return response()->json([
-            "user" => $user, 
+            "data" => $user, 
+            "token" => $token, 
+            "response" => 'insert user', 
         ], 201);
 
     }
