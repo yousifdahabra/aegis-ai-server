6e7bd942da6ff96d6689cac41901230c44e24dbb
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-16 22:23:44 +0200

    Back-end - update UserController add register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    function register(Request $request){
+        $user = new User;
+        $user->user_role_id = 1; 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone_number = $request->phone_number;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return response()->json([
+            "user" => $user, 
+        ], 201);
+
+    }
 }
