4b09e16257373a5c861e54ca8452fb56ad237140
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-16 16:19:40 +0200

    Back-end - update expert_requests

--- database/migrations/2024_12_16_141327_expert_requests.php
+++ database/migrations/2024_12_16_141327_expert_requests.php
@@ -6,19 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('expert_requests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('extra_informations');
+            $table->string('links');
+            $table->timestamp('created_at')->nullable();
+            $table->softDeletes();
+
+        });
+
+        Schema::create('expert_request_documents', function (Blueprint $table) {
+            $table->id();
+            $table->integer('expert_request_id');
+            $table->string('file_path');
+            $table->timestamp('created_at')->nullable();
+            $table->softDeletes();
+        });
+
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('expert_requests');
+        Schema::dropIfExists('expert_request_documents');
+
     }
 };
