147f8d602d648a335fcbedd688e29c83fd39cd02
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-16 21:59:58 +0200

    Back-end - update UserRole model add users relationship

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -3,15 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class UserRole extends Model
 {
     protected $fillable = [
         'title',
     ];
-
-    
-    
-
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 
 }
