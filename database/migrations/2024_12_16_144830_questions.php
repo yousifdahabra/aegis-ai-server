9607aada7d4a71494e45e4764150e731c2d7661b
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-16 16:51:50 +0200

    Back-end - update questions migration add questions table

--- database/migrations/2024_12_16_144830_questions.php
+++ database/migrations/2024_12_16_144830_questions.php
@@ -6,19 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
-        //
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('test_id');
+            $table->integer('type_id');
+            $table->integer('previous_question_id')->default(0);
+            $table->string('title');
+            $table->timestamp('created_at')->nullable();
+            $table->softDeletes();
+        });
+
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
-        //
+        Schema::dropIfExists('questions');
     }
 };
