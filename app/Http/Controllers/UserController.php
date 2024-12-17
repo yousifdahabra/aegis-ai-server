5dc18b0d3740a15d35b08c2404ba4fc49cf48f60
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-17 18:42:37 +0200

    Back-end - update  UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
-
+use Auth;
 class UserController extends Controller
 {
     function register(Request $request){
@@ -34,8 +34,28 @@ class UserController extends Controller
         return response()->json([
             "data" => $user, 
             "token" => $token, 
-            "response" => 'insert user', 
+            "message" => 'insert user', 
         ], 201);
-
     }
+    public function login(Request $request){
+        $credentials = $request->only('email', 'password');
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            $user = auth()->user();
+
+            return response()->json([
+                "user" => $user, 
+                "token" => $token,
+                "message" => 'login succ', 
+
+            ], 201);
+
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+    }
+
 }
