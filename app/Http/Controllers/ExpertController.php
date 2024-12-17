619a4828a74ebf151214b7774a14fdba2caf1640
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-18 01:40:49 +0200

    Back-end - update expert_requests migration update links type

--- app/Http/Controllers/ExpertController.php
+++ app/Http/Controllers/ExpertController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class ExpertController extends Controller
 {
-    //
+        function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'extra_informations' => 'required|string|max:255',
+            'links' => 'required|string|max:255',
+
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = new User;
+        $user->user_role_id = 1; 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone_number = $request->phone_number;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            "data" => $user, 
+            "token" => $token, 
+            "message" => 'insert user', 
+        ], 201);
+    }
+
 }
---
+++ database/migrations/2024_12_17_233902_alter_table_expert_requests_change_links.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void{
+        Schema::table('expert_requests', function (Blueprint $table) {
+            $table->text('links')->change();
+        });
+    }
+
+    public function down(): void{
+        Schema::table('expert_requests', function (Blueprint $table) {
+            $table->string('links')->change();
+        });
+    }
+};
