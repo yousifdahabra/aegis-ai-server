e4552781d5d4cba87ccd4e32af70d47a0e5d3c63
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 01:55:16 +0200

    Back-end - update expert_requests migration add timestamps

--- database/migrations/2024_12_18_235350_fix_expert_requests_add_timestamps.php
+++ database/migrations/2024_12_18_235350_fix_expert_requests_add_timestamps.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('expert_requests', function (Blueprint $table) {
-            //
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('expert_requests', function (Blueprint $table) {
-            //
+            $table->dropTimestamps();
         });
     }
 };
