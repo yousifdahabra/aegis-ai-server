d7dbf2e6276a2769b12559d3f9268e80eb88dde5
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 00:35:59 +0200

    Back-end - update UserService add register method

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -6,6 +6,13 @@ use App\Models\User;
 class UserService{
     public function register(array $data): User
     {
-
+        $user = new User;
+        $user->user_role_id = 1; 
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->phone_number = $data['phone_number'];
+        $user->password = Hash::make($data['password']);
+        $user->save();
+        return $user;
     }
 }
