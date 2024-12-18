69a02bb729eda938427124f6cb95bb921274ff37
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 00:15:30 +0200

    Back-end - update UserController update register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,32 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\auth\RegisterUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
 class UserController extends Controller
 {
-    function register(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'phone_number' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-        if($validator->fails()){
-            return response()->json($validator->errors()->toJson(), 400);
-        }
+    function register(RegisterUserRequest $request){
+        $data =  $request->validated();
 
         $user = new User;
         $user->user_role_id = 1; 
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->phone_number = $request->phone_number;
-        $user->password = Hash::make($request->password);
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->phone_number = $data['phone_number'];
+        $user->password = Hash::make($data['password']);
         $user->save();
         $token = JWTAuth::fromUser($user);
 
