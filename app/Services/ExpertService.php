fb8c8f9a2dcd3acc2ed2a60618b60598f41b9f3d
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 01:50:06 +0200

    Back-end - update ExpertService update store

--- app/Services/ExpertService.php
+++ app/Services/ExpertService.php
@@ -8,6 +8,10 @@ class ExpertService{
     public function store(array $data): ExpertRequest
     {
         $expert = new ExpertRequest;
+        $expert->user_id = $data['user_id'] ?? 1;
+        $expert->links = $data['links'];
+        $expert->extra_informations = $data['extra_informations'];
+        $expert->save();
 
         return $expert;
     }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ExpertController;
 use Illuminate\Support\Facades\Route;
 
 
 
+Route::post("/expert_register", [ExpertController::class, "register"]);
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
