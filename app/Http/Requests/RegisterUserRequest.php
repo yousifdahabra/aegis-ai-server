504432c7d74c26876a4a9b1a43865c3df7efa228
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-18 23:26:03 +0200

    Back-end - update RegisterUserRequest add messages

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -20,4 +20,19 @@ class RegisterUserRequest extends FormRequest
             'password' => 'required|string|min:6',
         ];
     }
+    public function messages(){
+        return [
+            'name.required' => 'The name field is required.',
+            'name.max' => 'The name field has a maximum 255 character limit.',
+            'phone_number.required' => 'The phone number field is required.',
+            'phone_number.max' => 'The phone_number field has a maximum 255 character limit.',
+            'email.required' => 'The email field is required.',
+            'email.email' => 'Please provide a valid email address.',
+            'email.unique' => 'This email is already registered.',
+            'password.required' => 'The password field is required.',
+            'password.min' => 'Password must be at least 8 characters long.',
+            'password.confirmed' => 'Password confirmation does not match.',
+        ];
+    }
+
 }
