988d9d301feff05fd8b213bec3a75d36de7adc90
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 01:49:11 +0200

    Back-end - update ExpertController add UserService and ExpertService

--- app/Http/Controllers/ExpertController.php
+++ app/Http/Controllers/ExpertController.php
@@ -3,27 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\Expert\RegisterExpertRequest;
+
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Services\UserService;
+use App\Services\ExpertService;
 
 class ExpertController extends Controller
 {
-        function register(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'phone_number' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-            'extra_informations' => 'required|string|max:255',
-            'links' => 'required|string',
+    protected $userService;
+    protected $expertService;
 
-        ]);
-        if($validator->fails()){
-            return response()->json($validator->errors()->toJson(), 400);
-        }
+    function __construct(UserService $userService,ExpertService $expertService){
+        $this->userService = $userService;
+        $this->expertService = $expertService;
+    }
 
+    function register(RegisterExpertRequest $request){
+        $data =  $request->validated();
+
+        
     
     }
 
