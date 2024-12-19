abef00090b4916e2a88feb300a89f725cf7eaa8e
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 23:44:16 +0200

    Back-end - update QuestionTypesSeeder insert question_types

--- database/seeders/QuestionTypesSeeder.php
+++ database/seeders/QuestionTypesSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\QuestionType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class QuestionTypesSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        QuestionType::insert([
+            ['title' => 'input'],
+            ['title' => 'radio'],
+            ['title' => 'check'],
+            ['title' => 'email'],
+            ['title' => 'install'],
+            ['title' => 'call'],
+        ]);
+
     }
 }
