07174b9f2a7ff5972b2a1e56f90e2d3550e49dbf
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 22:13:57 +0200

    Back-end - add a private disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,7 +56,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
-
+        'private' => [
+            'driver' => 'local',
+            'root'   => storage_path('app/private'),
+        ],
+    
     ],
 
     /*
