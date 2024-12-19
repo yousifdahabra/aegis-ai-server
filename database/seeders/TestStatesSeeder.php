2f5d7f9d8f5161955545cffb8a4dbd4b01507315
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-19 23:40:01 +0200

    Back-end - update TestStatesSeeder add test_states

--- database/seeders/TestStatesSeeder.php
+++ database/seeders/TestStatesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\TestState;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -9,6 +10,11 @@ class TestStatesSeeder extends Seeder
 {
     public function run(): void
     {
+        TestState::insert([
+            ['title' => 'Pending'],
+            ['title' => 'Progress'],
+            ['title' => 'Done'],
+        ]);
 
     }
 }
