4e39cd83b27d2833733a2fe07f796bd802e4450d
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-22 23:15:39 +0200

    Back-end - update bootstrap\app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\AuthMiddleware;
+use App\Http\Middleware\JwtMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(AuthMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
