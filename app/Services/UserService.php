5186d47f4d18b5cde51dfa3dfadd95fde0e8c6b0
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 20:04:11 +0200

    Back-end - update UserService add update user

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace App\Services;
 
 use App\Models\User;
@@ -9,7 +9,7 @@ class UserService{
     public function register(array $data): User
     {
         $user = new User;
-        $user->user_role_id = 3; 
+        $user->user_role_id = 3;
         $user->name = $data['name'];
         $user->email = $data['email'];
         $user->phone_number = $data['phone_number'];
@@ -17,4 +17,21 @@ class UserService{
         $user->save();
         return $user;
     }
+
+    public function update($data,$id): User
+    {
+        $user = User::findOrFail($id);
+        if(!$user){
+            return false;
+        }
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->phone_number = $data['phone_number'];
+        $user->password = Hash::make($data['password']);
+        $user->save();
+        return $user;
+    }
+
+
+
 }
