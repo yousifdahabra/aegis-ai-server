f5e8ecc65ed8c8822a714279359b84517134c06e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 18:37:52 +0200

    Back-end - update JwtMiddleware

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -10,12 +10,19 @@ class JwtMiddleware
 {
     public function handle($request, Closure $next)
     {
+
         try {
             $user = JWTAuth::parseToken()->authenticate();
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'Token not valid'], 401);
+            if (!$user) {
+                return response()->json(['error' => 'User not found'], 401);
+            }
+        } catch (TokenInvalidException) {
+            return response()->json(['error' => 'Token is invalid'], 401);
+        } catch (TokenExpiredException) {
+            return response()->json(['error' => 'Token has expired'], 401);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Authorization token not found'], 401);
         }
-        return response()->json(['error' => $request], 401);
 
         return $next($request);
     }
