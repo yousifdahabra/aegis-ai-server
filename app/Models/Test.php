1ec1dd41beef15ebf8c1465745963c77f0697cbe
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-26 11:41:42 +0200

    Back-end - update Test model add fillable

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
-    //
+
+    protected $fillable = [
+        'user_id',
+        'expert_id',
+        'test_state_id',
+        'title',
+    ];
+
 }
