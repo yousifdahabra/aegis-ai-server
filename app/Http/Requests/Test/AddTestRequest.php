d7e71602f45dd3509a92e537d25316b16cf66b6e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-26 15:10:11 +0200

    Back-end - update AddTestRequest add messages

--- app/Http/Requests/Test/AddTestRequest.php
+++ app/Http/Requests/Test/AddTestRequest.php
@@ -22,4 +22,23 @@ class AddTestRequest extends BaseRequest
             'test_state_id' => 'required|numeric|exists:test_states,id',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'The title is required.',
+            'title.string' => 'The title must be a string.',
+            'title.max' => 'The title may not be greater than 255 characters.',
+            'user_id.required' => 'The user ID is required.',
+            'user_id.numeric' => 'The user ID must be a number.',
+            'user_id.exists' => 'The selected user ID does not exist in the users table.',
+            'expert_id.required' => 'The expert ID is required.',
+            'expert_id.numeric' => 'The expert ID must be a number.',
+            'expert_id.exists' => 'The selected expert ID does not exist in the users table.',
+            'test_state_id.required' => 'The test state ID is required.',
+            'test_state_id.numeric' => 'The test state ID must be a number.',
+            'test_state_id.exists' => 'The selected test state ID does not exist in the test_states table.',
+        ];
+    }
+
 }
