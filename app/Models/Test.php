0cb1296cc5058a8e2a33eeb9f67e711aab3ab2bf
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-26 12:02:46 +0200

    Back-end - update Test model add BelongsTo user

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Test extends Model
 {
@@ -13,5 +14,9 @@ class Test extends Model
         'test_state_id',
         'title',
     ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
