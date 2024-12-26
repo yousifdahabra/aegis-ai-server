82bf18b7c70b72562fbf28140749d2e8b6279966
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-26 15:27:03 +0200

    Back-end - update TestService add store method

--- app/Services/TestService.php
+++ app/Services/TestService.php
@@ -4,4 +4,15 @@ namespace App\Services;
 use App\Models\Test;
 
 class TestService{
+
+    public function store(array $data): Test
+    {
+        $test = new Test;
+        $test->user_id = $data['user_id'];
+        $test->expert_id = $data['expert_id'];
+        $test->test_state_id = $data['test_state_id'];
+        $test->save();
+        return $test;
+    }
+
 }
