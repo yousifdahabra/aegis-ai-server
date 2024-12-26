71ed1d1b9cd4945fe50a1c6a6245f1be56a20771
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-26 15:37:53 +0200

    Back-end - update TestService add update method

--- app/Services/TestService.php
+++ app/Services/TestService.php
@@ -8,9 +8,21 @@ class TestService{
     public function store(array $data): Test
     {
         $test = new Test;
+        $test->title = $data['title'];
         $test->user_id = $data['user_id'];
         $test->expert_id = $data['expert_id'];
         $test->test_state_id = $data['test_state_id'];
+        $test->save();
+        return $test;
+    }
+
+    public function update(array $data,$id): Test
+    {
+        $test = Test::findOrFail($id);
+        if(!$test){
+            return false;
+        }
+        $test->title = $data['title'];
         $test->save();
         return $test;
     }
