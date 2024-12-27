d375baf5f1ee2e442d32cec820edb63160da587f
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 23:19:37 +0200

    Back-end - update UpdateQuestionRequest add rules

--- app/Http/Requests/Test/UpdateQuestionRequest.php
+++ app/Http/Requests/Test/UpdateQuestionRequest.php
@@ -4,25 +4,21 @@ namespace App\Http\Requests\Test;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateQuestionRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+class UpdateQuestionRequest extends FormRequest{
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'question_type_id' => 'required|numeric|exists:question_types,id',
+            'options' => 'required|array|min:1',
+            'options.*.id' => 'required|numeric',
+            'options.*.title' => 'required|string|max:255',
         ];
     }
 }
