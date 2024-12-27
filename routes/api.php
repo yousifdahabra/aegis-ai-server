229579dff2b9643c115edf9aae5e73108d4de286
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 13:53:55 +0200

    Back-end - update api route add tests store

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExpertController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
 use App\Http\Middleware\JwtMiddleware;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\ExpertMiddleware;
@@ -13,14 +16,21 @@ Route::post("/expert_register", [ExpertController::class, "register"]);
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
 
-Route::middleware([AdminMiddleware::class])->group(function () {
-    Route::prefix('users')->group(function () {
-        Route::get('/{user_id?}', [UserController::class, 'show']);
-        Route::put('/{user_id}', [UserController::class, 'update']);
-        Route::delete('/{user_id}', [UserController::class, 'destroy']);
+Route::middleware([ExpertMiddleware::class])->group(function () {
+    Route::middleware([ExpertMiddleware::class])->group(function () {
+        Route::prefix('users')->group(function () {
+            Route::get('/{id?}', [UserController::class, 'show']);
+            Route::put('/{id}', [UserController::class, 'update']);
+            Route::delete('/{id}', [UserController::class, 'destroy']);
+        });
+
+        Route::prefix('experts')->group(function () {
+            Route::get('/{id?}', [ExpertController::class, 'show']);
+            Route::delete('/{id}', [ExpertController::class, 'destroy']);
+        });
     });
-    Route::prefix('experts')->group(function () {
-        Route::get('/{expert_id?}', [ExpertController::class, 'show']);
-        Route::delete('/{expert_id}', [ExpertController::class, 'destroy']);
+    Route::prefix('tests')->group(function () {
+        Route::get('/{id?}', [TestController::class, 'store']);
     });
+
 });
