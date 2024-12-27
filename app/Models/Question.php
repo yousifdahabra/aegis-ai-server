4eb0fb57918ea1c704ebacbb762b41cdc6afb3b7
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 17:47:03 +0200

    Back-end - update Question model add fillable

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+
+    protected $fillable = [
+        'test_id',
+        'question_types_id',
+        'previous_question_id',
+        'title',
+    ];
+
 }
