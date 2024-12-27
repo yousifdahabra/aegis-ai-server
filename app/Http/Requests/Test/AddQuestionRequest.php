f81a9a76803a8ff380c48965d664ca82583cca8c
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 19:02:06 +0200

    Back-end - update AddQuestionRequest add messages

--- app/Http/Requests/Test/AddQuestionRequest.php
+++ app/Http/Requests/Test/AddQuestionRequest.php
@@ -17,7 +17,30 @@ class AddQuestionRequest extends FormRequest
             'title' => 'required|string|max:255',
             'test_id' => 'required|numeric|exists:tests,id',
             'question_type_id' => 'required|numeric|exists:question_types,id',
-            'previous_question_id' => 'numeric|exists:test_states,id',
+            'previous_question_id' => 'numeric|exists:questions,id',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'The title is required.',
+            'title.string' => 'The title must be a string.',
+            'title.max' => 'The title may not be greater than 255 characters.',
+            'test_id.required' => 'The test ID is required.',
+            'test_id.numeric' => 'The test ID must be a number.',
+            'test_id.exists' => 'The selected test ID does not exist in the tests table.',
+            'question_type_id.required' => 'The question type ID is required.',
+            'question_type_id.numeric' => 'The question type ID must be a number.',
+            'question_type_id.exists' => 'The selected question type ID does not exist in the question_types table.',
+            'previous_question_id.numeric' => 'The previous question ID must be a number.',
+            'previous_question_id.exists' => 'The selected previous question ID does not exist in the questions table.',
+        ];
+    }
+
+
+
+
+
+
 }
