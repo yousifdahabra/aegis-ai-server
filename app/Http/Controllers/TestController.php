371edaac1a3e97e8f0f8f5b9344c82d14249724d
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 13:28:46 +0200

    Back-end - update TestController add TestService

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Test\AddTestRequest;
+use App\Services\TestService;
 
 class TestController extends Controller
 {
-    //
+    protected $testService;
+
+    function __construct(TestService $testService){
+        $this->testService = $testService;
+    }
+
+
 }
