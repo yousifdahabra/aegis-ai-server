8042c045bd120673df5b758b9430ffe2003db331
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 22:47:36 +0200

    Back-end - update QuestionController update store add QuestionResource

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Test\AddQuestionRequest;
+use App\Http\Resources\QuestionResource;
 use Illuminate\Http\Request;
 use App\Services\QuestionService;
 
@@ -16,7 +17,12 @@ class QuestionController extends Controller
 
     function store(AddQuestionRequest $request){
         $data =  $request->validated();
-
+        $test = $this->question_service->store($data);
+        return response()->json([
+            'status' => true,
+            "data" => new QuestionResource($test),
+            "message" => 'Question created successfully',
+        ], 201);
     }
 
 }
