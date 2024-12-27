5411665effab359b9af04daebf9d543b58be6a5e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 14:27:00 +0200

    Back-end - update UpdateTestRequest add rules

--- app/Http/Requests/Test/UpdateTestRequest.php
+++ app/Http/Requests/Test/UpdateTestRequest.php
@@ -2,27 +2,19 @@
 
 namespace App\Http\Requests\Test;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseRequest;
 
-class UpdateTestRequest extends FormRequest
+class UpdateTestRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
         ];
     }
 }
