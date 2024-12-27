22e6bfac4dc3e856ee9e4b5e1984046527d7c910
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 19:35:18 +0200

    Back-end - update QuestionService add store with options

--- app/Services/QuestionService.php
+++ app/Services/QuestionService.php
@@ -5,4 +5,23 @@ use App\Models\Option;
 
 class QuestionService{
 
+    public function store(array $data): Question
+    {
+        $question = new Question;
+        $question->test_id = $data['test_id'];
+        $question->question_types_id = $data['question_types_id'];
+        $question->previous_question_id = $data['previous_question_id'] ?? 0;
+        $question->title = $data['title'];
+        $question->save();
+
+        foreach ($data['options'] as $option_data) {
+            $option = new Option;
+            $option->question_id = $question->id;
+            $option->title = $option_data['title'];
+            $option->save();
+        }
+
+        return $question;
+    }
+
 }
