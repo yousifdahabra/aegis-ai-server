455acec679ad69dea64fc366f5206729f156307e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-27 20:24:56 +0200

    Back-end - update OptionResource add toArray

--- app/Http/Resources/OptionResource.php
+++ app/Http/Resources/OptionResource.php
@@ -7,13 +7,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class OptionResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+        ];
     }
 }
