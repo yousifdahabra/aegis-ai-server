4f5ab6f0528bf349f3282ef20b5d663195caa5a7
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 01:53:54 +0200

    Back-end - update UpdateUserAnswersRequest add messages

--- app/Http/Requests/Test/UpdateUserAnswersRequest.php
+++ app/Http/Requests/Test/UpdateUserAnswersRequest.php
@@ -13,5 +13,12 @@ class UpdateUserAnswersRequest extends BaseRequest{
         ];
     }
 
+    public function messages(): array
+    {
+        return [
+            'option_answer.required' => 'The option answer field is required.',
+            'option_answer.string' => 'The option answer must be a valid string.',
+        ];
+    }
 
 }
