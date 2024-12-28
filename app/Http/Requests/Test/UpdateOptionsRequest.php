6b6b29086f10b732cc218d53cf21eed4cd0f0c7d
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 11:02:53 +0200

    Back-end - update UpdateOptionsRequest add messages

--- app/Http/Requests/Test/UpdateOptionsRequest.php
+++ app/Http/Requests/Test/UpdateOptionsRequest.php
@@ -16,4 +16,14 @@ class UpdateOptionsRequest extends BaseRequest{
             'title' => 'required|string|max:255',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'The title field is required.',
+            'title.string' => 'The title must be a string.',
+            'title.max' => 'The title may not be greater than 255 characters.',
+        ];
+    }
+
 }
