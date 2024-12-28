f7bede9aa6bbd6af6a6049db4711f577954fc523
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 11:07:06 +0200

    Back-end - update OptionController add OptionsService

--- app/Http/Controllers/OptionController.php
+++ app/Http/Controllers/OptionController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\OptionsService;
 use Illuminate\Http\Request;
 
 class OptionController extends Controller
 {
-    //
+    protected $option_service;
+
+    function __construct(OptionsService $option_service){
+        $this->option_service = $option_service;
+    }
+
+
 }
