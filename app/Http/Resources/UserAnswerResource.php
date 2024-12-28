93f5667467750c2bf958273583456423d3d8463f
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 20:38:42 +0200

    Back-end - update UserAnswerResource add toArray

--- app/Http/Resources/UserAnswerResource.php
+++ app/Http/Resources/UserAnswerResource.php
@@ -7,13 +7,11 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserAnswerResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+        ];
     }
 }
