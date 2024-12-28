fc00293fd229f71f211b0fcf69710f92e1afa431
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 20:25:33 +0200

    Back-end - update UserAnswerController add user_service

--- app/Http/Controllers/UserAnswerController.php
+++ app/Http/Controllers/UserAnswerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
-class UserAnswerController extends Controller
-{
-    //
+class UserAnswerController extends Controller{
+
+    protected $user_service;
+
+    function __construct(UserService $user_service){
+        $this->user_service = $user_service;
+    }
 }
