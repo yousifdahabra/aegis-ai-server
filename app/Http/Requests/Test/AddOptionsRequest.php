e15f35bc02507de4d07735509d906a964e375a47
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 11:01:03 +0200

    Back-end - update AddOptionsRequest add messages

--- app/Http/Requests/Test/AddOptionsRequest.php
+++ app/Http/Requests/Test/AddOptionsRequest.php
@@ -18,4 +18,15 @@ class AddOptionsRequest extends FormRequest
             'question_id' => 'required|numeric|exists:questions,id',
         ];
     }
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'The title field is required.',
+            'title.string' => 'The title must be a string.',
+            'title.max' => 'The title may not be greater than 255 characters.',
+            'question_id.required' => 'The question ID is required.',
+            'question_id.numeric' => 'The question ID must be a number.',
+            'question_id.exists' => 'The selected question does not exist.',
+        ];
+    }
 }
