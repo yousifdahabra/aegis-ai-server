ccc87b3018918ec904793a61e68bdd1ee45825a0
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 20:36:02 +0200

    Back-end - add UserAnswerResource

--- app/Http/Controllers/UserAnswerController.php
+++ app/Http/Controllers/UserAnswerController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\UserService;
+use App\Services\UserAnswersService;
 use Illuminate\Http\Request;
 
 class UserAnswerController extends Controller{
 
-    protected $user_service;
+    protected $user_answers_service;
 
-    function __construct(UserService $user_service){
-        $this->user_service = $user_service;
+    function __construct(UserAnswersService $user_answers_service){
+        $this->user_answers_service = $user_answers_service;
     }
+
+    public function show($id = 0){
+        $user_answers_service = $this->user_answers_service->get_user_answers($id);
+
+        if(!$user_answers_service){
+            return response()->json([
+                'status' => false,
+                'message' => 'Expert error',
+            ], 422);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Get Tests successfully',
+            'data' => TestResource::collection($tests),
+        ], 200);
+    }
+
 }
