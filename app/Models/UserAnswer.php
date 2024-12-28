0e27cf230b2a262cae5cbceb01a50d7931a57f23
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 18:32:44 +0200

    Back-end - update UserAnswer model add HasOne question

--- app/Models/UserAnswer.php
+++ app/Models/UserAnswer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class UserAnswer extends Model{
 
@@ -12,5 +13,10 @@ class UserAnswer extends Model{
         'question_id',
         'option_answer',
     ];
+
+    public function question(): HasOne
+    {
+        return $this->hasOne(Question::class);
+    }
 
 }
