7d0ab51e38d9e1ce277917b065bb507b5947a8e3
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 19:47:40 +0200

    Back-end - update UserAnswersService add update

--- app/Services/UserAnswersService.php
+++ app/Services/UserAnswersService.php
@@ -8,12 +8,23 @@ class UserAnswersService{
 
     public function store(array $data): UserAnswer
     {
-        $test = new UserAnswer;
-        $test->user_id = $data['user_id'];
-        $test->question_id = $data['question_id'];
-        $test->option_answer = $data['option_answer'];
-        $test->save();
-        return $test;
+        $user_answer = new UserAnswer;
+        $user_answer->user_id = $data['user_id'];
+        $user_answer->question_id = $data['question_id'];
+        $user_answer->option_answer = $data['option_answer'];
+        $user_answer->save();
+        return $user_answer;
     }
+
+    public function update(array $data,$id){
+        $user_answer = UserAnswer::find($id);
+        if(!$user_answer){
+            return false;
+        }
+        $user_answer->option_answer = $data['option_answer'];
+        $user_answer->save();
+        return $user_answer;
+    }
+
 
 }
