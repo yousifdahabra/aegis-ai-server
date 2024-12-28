d07d01399c4f906c4ae691ea602f1d573e48f1c8
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 10:54:35 +0200

    Back-end - update OptionsService add delete

--- app/Services/OptionsService.php
+++ app/Services/OptionsService.php
@@ -18,9 +18,19 @@ Class OptionsService{
         if (!$option) {
             return false;
         }
-
-
-
+        $option->title = $data['title'];
+        $option->save();
         return $option;
     }
+
+    public function delete($id){
+        $option = Option::find($id);
+        if (!$option) {
+            return false;
+        }
+        $option->delete();
+
+        return true;
+    }
+
 }
