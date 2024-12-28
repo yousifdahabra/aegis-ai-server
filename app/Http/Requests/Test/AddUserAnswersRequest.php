d22e954e80d040c1e92e5d930dbebe3b4da9361e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 01:52:19 +0200

    Back-end - update AddUserAnswersRequest add messages

--- app/Http/Requests/Test/AddUserAnswersRequest.php
+++ app/Http/Requests/Test/AddUserAnswersRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests\Test;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseRequest;
 
-class AddUserAnswersRequest extends FormRequest{
+class AddUserAnswersRequest extends BaseRequest{
 
     public function authorize(): bool
     {
@@ -19,4 +19,19 @@ class AddUserAnswersRequest extends FormRequest{
             'option_answer' => 'required|string',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'user_id.required' => 'The user ID is required.',
+            'user_id.numeric' => 'The user ID must be a valid number.',
+            'user_id.exists' => 'The specified user does not exist in our records.',
+            'question_id.required' => 'The question ID is required.',
+            'question_id.numeric' => 'The question ID must be a valid number.',
+            'question_id.exists' => 'The specified question does not exist in our records.',
+            'option_answer.required' => 'The option answer field is required.',
+            'option_answer.string' => 'The option answer must be a valid string.',
+        ];
+    }
+
 }
