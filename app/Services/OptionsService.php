bee7e7de3c0c3917b4a8035a067722e3959cdc3a
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 10:52:14 +0200

    Back-end - update OptionsService add update

--- app/Services/OptionsService.php
+++ app/Services/OptionsService.php
@@ -7,9 +7,20 @@ Class OptionsService{
     public function store(array $data): Option
     {
         $option = new Option;
+        $option->question_id = $data['question_id'];
         $option->title = $data['title'];
         $option->save();
         return $option;
     }
 
+    public function update(array $data, $id){
+        $option = Option::find($id);
+        if (!$option) {
+            return false;
+        }
+
+
+
+        return $option;
+    }
 }
