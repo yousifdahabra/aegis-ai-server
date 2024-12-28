fe91a1bd8fbb02711f08c2537f7bc642b08dec3d
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 10:44:10 +0200

    Back-end - update OptionsService add store

--- app/Services/OptionsService.php
+++ app/Services/OptionsService.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Models\Option;
+
 Class OptionsService{
+
+    public function store(array $data): Option
+    {
+        $option = new Option;
+
+        return $option;
+    }
 
 }
