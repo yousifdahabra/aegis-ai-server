4b6f65ca8cd44f8efd7eb7e3d43760e9b1f5ee42
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 01:50:59 +0200

    Back-end - update AddUserAnswersRequest add rules

--- app/Http/Requests/Test/AddUserAnswersRequest.php
+++ app/Http/Requests/Test/AddUserAnswersRequest.php
@@ -4,25 +4,19 @@ namespace App\Http\Requests\Test;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AddUserAnswersRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+class AddUserAnswersRequest extends FormRequest{
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'user_id' => 'required|numeric|exists:users,id',
+            'question_id' => 'required|numeric|exists:questions,id',
+            'option_answer' => 'required|string',
         ];
     }
 }
