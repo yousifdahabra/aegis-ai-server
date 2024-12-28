ce0dc6dd427d2f0b7b0959272e78a271cf81526a
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-28 21:31:53 +0200

    Back-end - update UserAnswerResource update toArray add QuestionResource

--- app/Http/Resources/UserAnswerResource.php
+++ app/Http/Resources/UserAnswerResource.php
@@ -5,13 +5,14 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserAnswerResource extends JsonResource
-{
+class UserAnswerResource extends JsonResource{
+
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
-            'title' => $this->title,
+            'option_answer' => $this->option_answer,
+            'question' => new QuestionResource($this->question),
         ];
     }
 }
