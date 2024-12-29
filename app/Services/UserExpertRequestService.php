fc545e335ffb933447e914d9413c0f3bf7e2fee6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 02:53:37 +0200

    Back-end - update UserExpertRequestService add get_user_expert_requests

--- app/Services/UserExpertRequestService.php
+++ app/Services/UserExpertRequestService.php
@@ -5,4 +5,15 @@ use App\Models\UserExpertRequest;
 
 class UserExpertRequestService{
 
+    public function get_user_expert_requests($id = 0){
+        if($id > 0){
+            $user_expert_request = UserExpertRequest::find($id);
+            if(!$user_expert_request){
+                return false;
+            }
+            return $user_expert_request;
+        }
+        return UserExpertRequest::all();
+    }
+
 }
