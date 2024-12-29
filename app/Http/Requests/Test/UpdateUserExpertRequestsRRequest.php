33f86c54c49d9ee2043dab2261e1fb6f14ecd33c
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 03:06:08 +0200

    Back-end - update UpdateUserExpertRequestsRRequest add messages

--- app/Http/Requests/Test/UpdateUserExpertRequestsRRequest.php
+++ app/Http/Requests/Test/UpdateUserExpertRequestsRRequest.php
@@ -19,4 +19,17 @@ class UpdateUserExpertRequestsRRequest extends BaseRequest{
             'links' => 'required|string',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'about_user.required' => 'The about user field is required.',
+            'about_user.string' => 'The about user must be a string.',
+            'user_note.required' => 'The user note field is required.',
+            'user_note.string' => 'The user note must be a string.',
+            'links.required' => 'The links field is required.',
+            'links.string' => 'The links must be a string.',
+        ];
+    }
+
 }
