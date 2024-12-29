d1b5a0de635d7cd8b43ea8242a33aed267137233
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 02:55:34 +0200

    Back-end - update UserExpertRequestService add store

--- app/Services/UserExpertRequestService.php
+++ app/Services/UserExpertRequestService.php
@@ -16,4 +16,15 @@ class UserExpertRequestService{
         return UserExpertRequest::all();
     }
 
+    public function store(array $data): UserExpertRequest
+    {
+        $user_expert_request = new UserExpertRequest;
+        $user_expert_request->user_id = $data['user_id'];
+        $user_expert_request->expert_id = $data['expert_id'];
+        $user_expert_request->about_user = $data['about_user'];
+        $user_expert_request->user_note = $data['user_note'];
+        $user_expert_request->save();
+        return $user_expert_request;
+    }
+
 }
