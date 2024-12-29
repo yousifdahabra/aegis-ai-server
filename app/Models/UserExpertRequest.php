c0eab106f65503ed9d329f97cead4def6c277560
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 02:45:39 +0200

    Back-end - update UserExpertRequest model add BelongsTo user

--- app/Models/UserExpertRequest.php
+++ app/Models/UserExpertRequest.php
@@ -3,12 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserExpertRequest extends Model{
 
     protected $fillable = [
-        'title',
+        'user_id',
+        'expert_id',
+        'about_user',
+        'user_note',
+        'links',
     ];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
