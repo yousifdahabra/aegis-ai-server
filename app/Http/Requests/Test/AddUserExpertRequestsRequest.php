227400e29e6cf19b95ab8340f120a2137034b754
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 03:04:30 +0200

    Back-end - update AddUserExpertRequestsRequest add messages

--- app/Http/Requests/Test/AddUserExpertRequestsRequest.php
+++ app/Http/Requests/Test/AddUserExpertRequestsRequest.php
@@ -22,4 +22,22 @@ class AddUserExpertRequestsRequest extends BaseRequest{
             'expert_id' => 'required|numeric|exists:users,id',
         ];
     }
+    public function messages(): array
+    {
+        return [
+            'about_user.required' => 'The about user field is required.',
+            'about_user.string' => 'The about user must be a string.',
+            'user_note.required' => 'The user note field is required.',
+            'user_note.string' => 'The user note must be a string.',
+            'links.required' => 'The links field is required.',
+            'links.string' => 'The links must be a string.',
+            'user_id.required' => 'The user ID field is required.',
+            'user_id.numeric' => 'The user ID must be a valid number.',
+            'user_id.exists' => 'The selected user ID does not exist in the database.',
+            'expert_id.required' => 'The expert ID field is required.',
+            'expert_id.numeric' => 'The expert ID must be a valid number.',
+            'expert_id.exists' => 'The selected expert ID does not exist in the database.',
+        ];
+    }
+
 }
