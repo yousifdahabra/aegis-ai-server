bf97723e9d5c349754a0d32482829580d8a5f685
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 02:44:15 +0200

    Back-end - update UserExpertRequest model add fillable

--- app/Models/UserExpertRequest.php
+++ app/Models/UserExpertRequest.php
@@ -4,7 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserExpertRequest extends Model
-{
-    //
+class UserExpertRequest extends Model{
+
+    protected $fillable = [
+        'title',
+    ];
+
+
 }
