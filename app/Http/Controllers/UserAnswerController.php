1f9b63ad0794a243e8f8395d2114dc4b188db963
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 02:05:56 +0200

    Back-end - update UserAnswerController add update

--- app/Http/Controllers/UserAnswerController.php
+++ app/Http/Controllers/UserAnswerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Test\AddUserAnswersRequest;
+use App\Http\Requests\Test\UpdateUserAnswersRequest;
 use App\Http\Resources\UserAnswerResource;
 use App\Services\UserAnswersService;
 use Illuminate\Http\Request;
@@ -21,13 +22,13 @@ class UserAnswerController extends Controller{
         if(!$user_answers_service){
             return response()->json([
                 'status' => false,
-                'message' => 'Expert error',
+                'message' => 'User Answer error',
             ], 422);
         }
 
         return response()->json([
             'status' => true,
-            'message' => 'Get Tests successfully',
+            'message' => 'Get User Answer successfully',
             'data' => UserAnswerResource::collection($user_answers_service),
         ], 200);
     }
@@ -38,8 +39,21 @@ class UserAnswerController extends Controller{
         return response()->json([
             'status' => true,
             "data" => new UserAnswerResource($user_answers_service),
-            "message" => 'Test created successfully',
+            "message" => 'User Answer created successfully',
         ], 201);
     }
+
+    function update(UpdateUserAnswersRequest $request,$id){
+        if(empty($id) || !is_numeric($id)){
+            return response()->json([
+                'status' => false,
+                "message" => 'User Answer Error',
+            ], 422);
+        }
+
+        $data =  $request->validated();
+        $user_answers_service = $this->user_answers_service->update($data,$id);
+
+     }
 
 }
