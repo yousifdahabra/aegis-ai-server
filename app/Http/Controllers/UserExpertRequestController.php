6b547b2e8e028203f33817d5ca60338c09845ba0
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 03:09:17 +0200

    Back-end - update UserExpertRequestController add UserExpertRequestService

--- app/Http/Controllers/UserExpertRequestController.php
+++ app/Http/Controllers/UserExpertRequestController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Test\AddUserExpertRequestsRequest;
+use App\Http\Requests\Test\UpdateUserExpertRequestsRRequest;
+use App\Http\Resources\UserExpertRequestResource;
+use App\Services\UserExpertRequestService;
 
-class UserExpertRequestController extends Controller
-{
-    //
+class UserExpertRequestController extends Controller{
+    protected $user_expert_request;
+
+    function __construct(UserExpertRequestService $user_expert_request){
+        $this->user_expert_request = $user_expert_request;
+    }
+
 }
