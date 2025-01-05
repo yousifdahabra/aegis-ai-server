46c954254d68e9d00d1425c8496151bf736b893b
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 16:46:28 +0200

    Back-end - update LoginRequest add messages

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -14,8 +14,20 @@ class LoginRequest extends BaseReques{
     public function rules(): array
     {
         return [
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|max:255',
             'password' => 'required|string|min:6',
         ];
     }
+
+    public function messages(){
+        return [
+            'email.required' => 'The email field is required.',
+            'email.email' => 'Please provide a valid email address.',
+            'email.max' => 'The email may not be greater than 255 characters.',
+            'password.required' => 'The password field is required.',
+            'password.min' => 'Password must be at least 8 characters long.',
+            'password.confirmed' => 'Password confirmation does not match.',
+        ];
+    }
+
 }
