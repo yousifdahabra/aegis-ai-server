b8b5ef463366c3f493bab84df5778d6d5cfa5b3c
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-06 17:54:42 +0200

    Back-end - update AdminMiddleware update error  return

--- app/Http/Middleware/ExpertMiddleware.php
+++ app/Http/Middleware/ExpertMiddleware.php
@@ -14,17 +14,32 @@ class ExpertMiddleware
         try {
             $user = JWTAuth::parseToken()->authenticate();
             if (!$user) {
-                return response()->json(['error' => 'User not found'], 401);
+                return response()->json([
+                    'status' => false,
+                    'message' => 'User not found',
+                ], 401);
             }
-            if($user['user_role_id'] == 3){
-                return response()->json(['error' => 'You are not Authorization'], 401);
+            if ($user['user_role_id'] == 3) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'You are not authorized',
+                ], 401);
             }
         } catch (TokenInvalidException) {
-            return response()->json(['error' => 'Token is invalid'], 401);
+            return response()->json([
+                'status' => false,
+                'message' => 'Token is invalid',
+            ], 401);
         } catch (TokenExpiredException) {
-            return response()->json(['error' => 'Token has expired'], 401);
+            return response()->json([
+                'status' => false,
+                'message' => 'Token has expired',
+            ], 401);
         } catch (Exception $e) {
-            return response()->json(['error' => 'Authorization token not found'], 401);
+            return response()->json([
+                'status' => false,
+                'message' => 'Authorization token not found',
+            ], 401);
         }
 
         return $next($request);
