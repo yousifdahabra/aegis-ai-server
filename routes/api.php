1a99eba561e4a15271fa51ad21cab0aeaa6d8bc6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-06 17:42:38 +0200

    Back-end - update api route rewrite  admin routes

--- routes/api.php
+++ routes/api.php
@@ -20,52 +20,50 @@ Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
 Route::post("/check-token-expiry", [UserController::class, "check_token_expiry"]);
 
-Route::middleware([JwtMiddleware::class])->group(function () {
-    Route::middleware([ExpertMiddleware::class])->group(function () {
-        Route::middleware([AdminMiddleware::class])->group(function () {
-            Route::prefix('users')->group(function () {
-                Route::get('/{id?}', [UserController::class, 'show']);
-                Route::put('/{id}', [UserController::class, 'update']);
-                Route::delete('/{id}', [UserController::class, 'destroy']);
-            });
+Route::middleware([AdminMiddleware::class])->group(function () {
+    Route::prefix('users')->group(function () {
+        Route::get('/{id?}', [UserController::class, 'show']);
+        Route::put('/{id}', [UserController::class, 'update']);
+        Route::delete('/{id}', [UserController::class, 'destroy']);
+    });
 
-            Route::prefix('experts')->group(function () {
-                Route::get('/{id?}', [ExpertController::class, 'show']);
-                Route::delete('/{id}', [ExpertController::class, 'destroy']);
-            });
-        });
-        Route::prefix('tests')->group(function () {
-            Route::get('/{id?}', [TestController::class, 'show']);
-            Route::post('/', [TestController::class, 'store']);
-            Route::put('/{id}', [TestController::class, 'update']);
-            Route::delete('/{id}', [TestController::class, 'destroy']);
-        });
-        Route::prefix('questions')->group(function () {
-            Route::get('/{id?}', [QuestionController::class, 'show']);
-            Route::post('/', [QuestionController::class, 'store']);
-            Route::put('/{id}', [QuestionController::class, 'update']);
-            Route::delete('/{id}', [QuestionController::class, 'destroy']);
-        });
+    Route::prefix('experts')->group(function () {
+        Route::get('/{id?}', [ExpertController::class, 'show']);
+        Route::delete('/{id}', [ExpertController::class, 'destroy']);
+    });
 
-        Route::prefix('options')->group(function () {
-            Route::get('/{questions_id}/{id?}', [OptionController::class, 'show']);
-            Route::post('/', [OptionController::class, 'store']);
-            Route::put('/{questions_id}/{id}', [OptionController::class, 'update']);
-            Route::delete('/{questions_id}/{id}', [OptionController::class, 'destroy']);
-        });
+    Route::prefix('tests')->group(function () {
+        Route::get('/{id?}', [TestController::class, 'show']);
+        Route::post('/', [TestController::class, 'store']);
+        Route::put('/{id}', [TestController::class, 'update']);
+        Route::delete('/{id}', [TestController::class, 'destroy']);
+    });
+    Route::prefix('questions')->group(function () {
+        Route::get('/{id?}', [QuestionController::class, 'show']);
+        Route::post('/', [QuestionController::class, 'store']);
+        Route::put('/{id}', [QuestionController::class, 'update']);
+        Route::delete('/{id}', [QuestionController::class, 'destroy']);
+    });
+    Route::prefix('options')->group(function () {
+        Route::get('/{questions_id}/{id?}', [OptionController::class, 'show']);
+        Route::post('/', [OptionController::class, 'store']);
+        Route::put('/{questions_id}/{id}', [OptionController::class, 'update']);
+        Route::delete('/{questions_id}/{id}', [OptionController::class, 'destroy']);
+    });
 
-        Route::prefix('user_answers')->group(function () {
-            Route::get('/{id?}', [UserAnswerController::class, 'show']);
-            Route::post('/', [UserAnswerController::class, 'store']);
-            Route::put('/{id}', [UserAnswerController::class, 'update']);
-            Route::delete('/{id}', [UserAnswerController::class, 'destroy']);
-        });
+    Route::prefix('user_answers')->group(function () {
+        Route::get('/{id?}', [UserAnswerController::class, 'show']);
+        Route::post('/', [UserAnswerController::class, 'store']);
+        Route::put('/{id}', [UserAnswerController::class, 'update']);
+        Route::delete('/{id}', [UserAnswerController::class, 'destroy']);
+    });
 
-        Route::prefix('user_expert_requests')->group(function () {
-            Route::get('/{id?}', [UserExpertRequestController::class, 'show']);
-            Route::post('/', [UserExpertRequestController::class, 'store']);
-            Route::put('/{id}', [UserExpertRequestController::class, 'update']);
-            Route::delete('/{id}', [UserExpertRequestController::class, 'destroy']);
-        });
+    Route::prefix('user_expert_requests')->group(function () {
+        Route::get('/{id?}', [UserExpertRequestController::class, 'show']);
+        Route::post('/', [UserExpertRequestController::class, 'store']);
+        Route::put('/{id}', [UserExpertRequestController::class, 'update']);
+        Route::delete('/{id}', [UserExpertRequestController::class, 'destroy']);
     });
 });
+
+
