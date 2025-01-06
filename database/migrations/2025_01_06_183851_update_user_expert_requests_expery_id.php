25446c246cdb5b57d6ebba18d5497f8c7fa732ce
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-06 20:41:07 +0200

    Back-end - update migration update_user_expert_requests_expery_id update up and down

--- database/migrations/2025_01_06_183851_update_user_expert_requests_expery_id.php
+++ database/migrations/2025_01_06_183851_update_user_expert_requests_expery_id.php
@@ -9,11 +9,15 @@ return new class extends Migration
 
     public function up(): void
     {
-
+        Schema::table('user_expert_requests', function (Blueprint $table) {
+            $table->integer('expert_id')->nullable()->default('0')->change();
+        });
     }
 
     public function down(): void
     {
-
+        Schema::table('user_expert_requests', function (Blueprint $table) {
+            $table->integer('expert_id')->default(null)->change();
+        });
     }
 };
