da44f71ec78b7379b8a0da95b8a1eb97c6c1e0ce
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-06 22:45:45 +0200

    Back-end - update migration add_tests_grade add security_grade

--- database/migrations/2025_01_06_202233_add_tests_grade.php
+++ database/migrations/2025_01_06_202233_add_tests_grade.php
@@ -6,23 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('tests', function (Blueprint $table) {
-            //
+            $table->string('security_grade')->nullable()->default('-');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('tests', function (Blueprint $table) {
-            //
+            $table->dropColumn('security_grade');
         });
     }
+
 };
