94a920739f4dcec3db910901ccf3bbc9de2762ed
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-08 22:27:23 +0200

    Back-end - update ChatGPTService add ask_chatgpt method and use OpenAI

--- app/Services/ChatGPTService.php
+++ app/Services/ChatGPTService.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace App;
+namespace App\Services;
+
+use OpenAI;
 
 class ChatGPTService{
 
-    public function __construct()
+    protected $client;
+
+    public function __construct(){
+        $this->client = OpenAI::client(config('services.openai.key'));
+    }
+
+    public function ask_chatgpt(string $message, array $context = []): string
     {
-        //
+        return "";
     }
 }
