ff16d018873e76322641005cb628dce5451494c8
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-09 07:57:00 +0200

    Back-end - update migration add_question_provider_id_and_use_question_id add question_provider_id and use_question_id

--- database/migrations/2025_01_09_055257_add_question_provider_id_and_use_question_id.php
+++ database/migrations/2025_01_09_055257_add_question_provider_id_and_use_question_id.php
@@ -4,25 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration{
+
     public function up(): void
     {
         Schema::table('questions', function (Blueprint $table) {
-            //
+            $table->integer('question_provider_id')->default(1);
+            $table->integer('use_question_id')->default(0);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('questions', function (Blueprint $table) {
-            //
+            $table->dropColumn('question_provider_id');
+            $table->dropColumn('use_question_id');
         });
     }
 };
