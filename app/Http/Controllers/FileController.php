d7dbd090947980222b3484fe0b25094483306ba6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 00:20:14 +0200

    Back-end - update FileController update show add exists

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,6 +9,13 @@ class FileController extends Controller{
     public function show($userId, $fileName){
         $filePath = "expert_certificates/{$userId}/{$fileName}";
 
+        if (!Storage::disk('private')->exists($filePath)) {
+            return response()->json(['error' => 'File not found.'], 404);
+        }
+
+        return response()->file(Storage::disk('private')->path($filePath), [
+            'Content-Disposition' => 'inline',
+        ]);
     }
 
 }
