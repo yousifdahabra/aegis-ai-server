7845587a58be5f6be1cb92e06295bc1d54c4fd3b
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 00:19:51 +0200

    Back-end - update FileController add show

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class FileController extends Controller
-{
-    //
+class FileController extends Controller{
+
+    public function show($userId, $fileName){
+        $filePath = "expert_certificates/{$userId}/{$fileName}";
+
+    }
+
 }
