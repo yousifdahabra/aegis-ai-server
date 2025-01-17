48729d8f6ec113fc15f3e0c295ca69f0a5e565cc
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 00:26:59 +0200

    Back-end - update FileController add download

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -10,12 +10,22 @@ class FileController extends Controller{
         $filePath = "expert_certificates/{$userId}/{$fileName}";
 
         if (!Storage::disk('private')->exists($filePath)) {
-            return response()->json(['error' => 'File not found.'], 404);
+            return response()->json([
+                'status' => false,
+                'message' => 'File not found',
+            ], 404);
+
         }
 
         return response()->file(Storage::disk('private')->path($filePath), [
             'Content-Disposition' => 'inline',
         ]);
+    }
+
+    public function download($userId, $fileName){
+        $filePath = "expert_certificates/{$userId}/{$fileName}";
+
+         return response()->download(Storage::disk('private')->path($filePath));
     }
 
 }
