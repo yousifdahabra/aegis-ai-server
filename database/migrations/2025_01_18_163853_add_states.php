16e7866383a22bd4e1cb94361b320e6f86f3d1ac
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 18:41:07 +0200

    Back-end - update migration add_states add state

--- database/migrations/2025_01_18_163853_add_states.php
+++ database/migrations/2025_01_18_163853_add_states.php
@@ -4,25 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration{
+
     public function up(): void
     {
         Schema::table('user_expert_requests', function (Blueprint $table) {
-            //
+            $table->integer('state')->default(0);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('user_expert_requests', function (Blueprint $table) {
-            //
+            $table->dropColumn('state');
         });
     }
 };
